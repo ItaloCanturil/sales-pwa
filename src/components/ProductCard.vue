cdcface09f1e0f3b579709fd04eeef3fcbd7d40e
Italo Canturil <italo.cantur@gmail.com>
2025-07-18 19:53:26 -0300

    feat: Add mock products

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-card class="my-card q-ma-sm">
+    <q-img
+      :src="product.image"
+      class="product-image"
+      :ratio="1"
+    />
+
+    <q-card-section class="text-left q-pa-md">
+      <div class="text-h6 ellipsis">{{ product.name }}</div>
+      <div class="text-subtitle2 text-weight-light q-mt-sm" @click="addProduct">
+        {{ product.price }}
+      </div>
+    </q-card-section>
+
+    <q-card-action>
+        <q-btn class="full-width" color="primary" label="Adicionar ao Carrinho"/>
+    </q-card-action>
+  </q-card>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapActions('cart', ['addProduct']),
+  }
+}
+</script>
+
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 300px;
+  transition: transform 0.3s;
+}
+
+.my-card:hover {
+  transform: translateY(-5px);
+}
+
+.product-image {
+  width: 100%;
+  object-fit: cover;
+}
+</style>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,10 +1,51 @@
 <template>
-    <div>Products</div>
+    <q-page class="row q-pa-md">
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="product in products" :key="product.id">
+    <product-card :product="product" />
+  </div>
+</q-page>
 </template>
 
 <script>
+import ProductCard from 'src/components/ProductCard.vue'
 export default {
-
+  components: { ProductCard },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Product 1',
+          price: 10.99,
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          price: 19.99,
+        },
+        {
+          id: 3,
+          name: 'Product 3',
+          price: 29.99,
+        },
+        {
+          id: 4,
+          name: 'Product 4',
+          price: 39.99,
+        },
+        {
+          id: 5,
+          name: 'Product 5',
+          price: 49.99,
+        },
+        {
+          id: 6,
+          name: 'Product 6',
+          price: 59.99,
+        },
+      ]
+    }
+  }
 }
 </script>
 
